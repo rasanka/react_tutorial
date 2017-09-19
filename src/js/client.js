98ef8a033b0a2dc932c32a3496bcb0430285cab6
Rasanka Jayawardana <rasanka.jayawardana@9spokes.com>
2017-09-19 22:02:57 +0000

    Basic React tutorial

--- src/js/client.js
+++ src/js/client.js
@@ -1,34 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./components/Layout";
-
-// Lesson #04 Completed
-
-/*
-class Layout extends React.Component {
-
-	constructor() {
-		super();
-		this.name = "Dilini";
-	}
-
-	getVal() {
-		return "Yippi"
-	}
-
-	render() {
-		var val = 1;
-		return (
-			<div>
-				<h1>Rasa...{3+2} Its working fine {val}!</h1>
-				<h1>Rasa... Its working fine {this.getVal()}</h1>
-				<h1>{this.name}</h1>
-			</div>
-		);
-	}
-}
-*/
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Layout from "./pages/Layout";
+import Settings from "./pages/Settings";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+app);
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,17 @@ var path = require('path');
 
 module.exports = {
   context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : false,
+  devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/client.js",
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/, 
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
         }
       }
     ]
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };
